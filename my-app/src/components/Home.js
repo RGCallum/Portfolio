03de822bd11a8bff3fb9ab2ef88bb835e27c7c0e
INDIA GUY <indiaguy@SCENEITs-iMac.local>
2018-12-28 03:58:06 -0500

    added some birds to home page

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -27,8 +27,7 @@ repeat 0 0;
 `
 
 const Bus = styled.div`
-
-    margin-top: 60%;
+    margin-top: 40%;
     position: absolute;
     z-index: 200;
     margin-left: 100%;
@@ -39,12 +38,23 @@ const Bus = styled.div`
 }
 
 `
+const Birds = styled.div`
+position: absolute;
+z-index: 200;
+margin-top: -5%;
+margin-left: 100%;
+animation: slide4 20s linear infinite;
+@keyframes slide4 {
+from { margin-left: 150%; }
+to { margin-left: -110%;  }
+}
+`
 
 const Child = styled.div`
 background-image: url(${camGirl});
 background-repeat: no-repeat;
 background-size: 17%;
-margin-top: 58%;
+margin-top: 36%;
 position: absolute;
 z-index: 100;
 height: 100%;
@@ -72,12 +82,16 @@ class Home extends Component {
 
                     </div>
                 </Container>
+                <Birds>
+                    <img src="https://vignette.wikia.nocookie.net/animaljam/images/9/93/Tumblr_ms4th0h51A1sfk0ybo1_500.gif/revision/latest?cb=20160416200533" alt="birds"/>
+                </Birds>
                 <Child>
 
                 </Child>
                 <Bus>
                     <img src="https://i.ibb.co/qrWcHwt/bus.png" alt="bus" />
                 </Bus>
+
             </div>
         );
     }
