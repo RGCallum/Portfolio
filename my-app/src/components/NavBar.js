69928929a861aad79b43afe2a394b80b8ea47e2f
INDIA GUY <indiaguy@SCENEITs-iMac.local>
2018-12-31 16:57:52 -0500

    hamburgered nav bar

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,49 +1,73 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
-import { BrowserRouter } from "react-router-dom";
 
 const NavStyle = styled.div`
 position: absolute;
 z-index: 1000;
-display: flex;
-flex-direction: row;
-justify-content: space-around;
-word-spacing: 80px;
-background-color: rgba(0, 0, 0, 0.1);
-width: 100vw;
-text-shadow: 1px 1px 1px black;
-a:link{
-    text-decoration: none;
-}
-a:visited{
-    color: white;
-}
-
-a:active{
-    color:  #23A6D5;
-}
-
-a:hover{
-    color:  #23A6D5;
-}
+max-width: 800px;
+margin: auto;
+margin-top: 1em;
+margin-left: 85vw;
+overflow: hidden;
+background-color: rgba(255, 255, 255, 0.619);
+border-radius: 5px;
+nav ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  
+  nav {
+    float: right;
+    a {
+      background: #000;
+      margin: .5em 0;
+      font-size: 6px;
+      transition: all 0.3s ease; 
+      text-transform: uppercase;
+      display: block;
+      color: #000;
+      text-decoration: none;
+    }
+    li:nth-child(n+4) {
+      a {
+        font-size: 0;
+      }
+    }
+    &:hover {
+      a {
+        background: rgba(255, 255, 255, 0.2);
+        font-size: 16px !important;
+        padding: 3px;
+        &:hover {
+          color: #23A6D5;
+          background: #000;
+        }
+      }
+    }
+  }
 `
 
 class NavBar extends Component {
     render() {
         return (
             <div>
+
                 <NavStyle>
-                    
-                    <BrowserRouter>
-                        <div>
-                            <h2>
-                            <a href="/"><i class="fas fa-home"></i>Home </a>
-                            <a href="/portfolio"><i class="far fa-images"></i>Portfolio </a>
-                            <a href="/skills"><i class="fas fa-magic"></i>Skills </a>
-                            <a href="/resume"><i class="far fa-list-alt"></i>Resume </a>        
-                            </h2>                 
-                        </div>
-                    </BrowserRouter>
+
+                    <div>
+                        <h2>
+                            <nav>
+                                <ul>
+                                    <li><a href="/"><i class="fas fa-home"></i> Home </a></li>
+                                    <li><a href="/portfolio"><i class="far fa-images"></i> Portfolio </a></li>
+                                    <li><a href="/skills"><i class="fas fa-magic"></i> Skills </a></li>
+                                    <li><a href="/resume"><i class="far fa-list-alt"></i> Resume </a> </li>
+                                </ul>
+                            </nav>
+                        </h2>
+                    </div>
                 </NavStyle>
             </div>
         );
