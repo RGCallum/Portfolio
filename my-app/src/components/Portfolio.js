4bf8729b9fe560af7958982890a68926dac394db
INDIA GUY <indiaguy@SCENEITs-iMac.local>
2019-01-05 04:05:14 -0500

    made changes falling alseep

--- my-app/src/components/Portfolio.js
+++ my-app/src/components/Portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Coverflow from 'react-coverflow';
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import {StyleRoot} from 'radium';
+import { StyleRoot } from 'radium';
 
 var fn = function () {
     /* do your action */
@@ -18,13 +18,22 @@ color: white;
 text-shadow: 1px 1px 1px black;
 h1{
     font-size: 45px;
-font-weight: 600;
+font-weight: 100;
+
 }
+h2{
+    text-align: center
+    background-color: rgba(0, 0, 0, 0.509);
+    padding: 10px;
+}
+
 alt{
     color: red;
 }
 background-size: 400% 400%;
-
+img{
+    width: 100%;
+}
 animation: Gradient 15s ease infinite;
 @keyframes Gradient {
 	0% {
@@ -37,71 +46,71 @@ animation: Gradient 15s ease infinite;
 		background-position: 0% 50%
 	}
 }
-body{
-    font-family: sans-serif;
-  }
-  h1{
-    font-weight: 100;
-  }
-  .container{
-    width: 100%;
-    display:block;
-    overflow:hidden;
-  }
-  .carousel{
-    display:block;
-    width: 100%;
-    height: 400px;
-    background: white;
-    overflow-x: scroll;
-    padding: 10px;
-    margin: 0;
-    white-space: nowrap; 
-    border-top: 2px solid rgba(0, 0, 0, 0.1);
-    border-bottom: 2px solid rgba(0, 0, 0, 0.1);
-  }
-  .item {
-      display: inline-block;
-      width: 380px;
-      margin: 0 10px;
-      height: 380px;
-      background: rgba(0, 0, 0, 0.05) no-repeat center center;
-      background-size: cover;
-      position:relative;
+// body{
+//     font-family: sans-serif;
+//   }
+//   h1{
+//     font-weight: 100;
+//   }
+//   .container{
+//     width: 100%;
+//     display:block;
+//     overflow:hidden;
+//   }
+//   .carousel{
+//     display:block;
+//     width: 100%;
+//     height: 400px;
+//     background: white;
+//     overflow-x: scroll;
+//     padding: 10px;
+//     margin: 0;
+//     white-space: nowrap; 
+//     border-top: 2px solid rgba(0, 0, 0, 0.1);
+//     border-bottom: 2px solid rgba(0, 0, 0, 0.1);
+//   }
+//   .item {
+//       display: inline-block;
+//       width: 380px;
+//       margin: 0 10px;
+//       height: 380px;
+//       background: rgba(0, 0, 0, 0.05) no-repeat center center;
+//       background-size: cover;
+//       position:relative;
       
-  }
-  .item div{
-    width: 100%;
-    top:10%;
-    position:absolute;
-    text-align:center;
-    font-size: 9em;
-    color: white;
-  }
-  .item h1{
-    top: 50%;
-    position: absolute;
-    color: white;
-    text-align: center;
-    width: 100%;
-    font-size: 4em;
-  }
-  #party{
-    background-image: url(https://i.ibb.co/ZWD3mL4/sceneitweb.png);
-  }
-  #mood{
-    background-image:url(https://i.ibb.co/s3FcP6n/Major-Keys.png);
-  }
-  #world{
-     background-image: url(http://www.whitehat-ops.com/media/cached/photos/9/640x640/constrain/90/Tendenci-Stock-Images-by-Ed-Schipul-2011-82-6ff50_medium_640-a38fa.jpg);
-  }
-  #ambient{
-    background-image:url(http://www.pdpics.com/preview/preview6/1070-orange-sky-sunset.jpg)
-  }
-  #work{
-    background-image:url(http://www.bianchii.com/img/background2.jpg)
-  }
-`
+//   }
+//   .item div{
+//     width: 100%;
+//     top:10%;
+//     position:absolute;
+//     text-align:center;
+//     font-size: 9em;
+//     color: white;
+//   }
+//   .item h1{
+//     top: 50%;
+//     position: absolute;
+//     color: white;
+//     text-align: center;
+//     width: 100%;
+//     font-size: 4em;
+//   }
+//   #party{
+//     background-image: url(https://i.ibb.co/ZWD3mL4/sceneitweb.png);
+//   }
+//   #mood{
+//     background-image:url(https://i.ibb.co/s3FcP6n/Major-Keys.png);
+//   }
+//   #world{
+//      background-image: url(http://www.whitehat-ops.com/media/cached/photos/9/640x640/constrain/90/Tendenci-Stock-Images-by-Ed-Schipul-2011-82-6ff50_medium_640-a38fa.jpg);
+//   }
+//   #ambient{
+//     background-image:url(http://www.pdpics.com/preview/preview6/1070-orange-sky-sunset.jpg)
+//   }
+//   #work{
+//     background-image:url(http://www.bianchii.com/img/background2.jpg)
+//   }
+// `
 const NextBtn = styled.div`
 button{
     display: flex ;
@@ -132,54 +141,83 @@ export default class Portfolio extends React.Component {
     render() {
         return (
             <div>
-                
+
                 <Container>
-                    <br/>
+                    <br />
                     <h1>Portfolio</h1>
-                    <h2>Click thumbnail to view Apps</h2>
-                
-                <StyleRoot>
-                <Coverflow
-                    width={560}
-                    height={730}
-                    displayQuantityOfSide={1.4}
-                    navigation={false}
-                    enableHeading={false}
-                    active={this.state.active}     
-                    
-                >
+                    <h3>Click thumbnail to view Apps</h3>
 
-                    <div
-                        onClick={() => fn()}
-                        onKeyDown={() => fn()}
-                        role="menuitem"
-                        tabIndex="0"
-                    >
-                    {/* Scroll or click Next button to view my work */}
-                    </div>
-                        {/* <div> 
-                            <h2>  SceneIt Pictures Interactive Website: <br /> HTML|CSS|JS </h2>                   */}
-                                 <img src='https://i.ibb.co/ZWD3mL4/sceneitweb.png' alt='SceneIt Pictures Interactive Website: HTML|CSS|JS' data-action="https://sceneitpix.com" /> 
-                        {/* </div> */}
-                    <img src='https://i.ibb.co/s3FcP6n/Major-Keys.png' alt='Major Keys Javacript Trivia Game: HTML|CSS|JS' data-action="https://majorkeys.netlify.com" />
-                    <img src='https://i.ibb.co/wY2FYNY/Callum-Ent.png' alt='Callum Enterprise website: HTML|CSS|JS' data-action="https://callumenterprise.com" />
-                    
-                    {/* <div>
-                            <h2> Social Media Network for Creative Freelancers:  <br /> JS|Python|React|Django|SQL</h2> */}
-                    <img src='https://i.ibb.co/5RwrNpJ/CCXMobile.png' alt='Social Media Network for Creative Freelancers: JS|Python|React|Django|SQL' data-action="https://creative-freedom.herokuapp.com"/>
-                    {/* </div> */}
+                    <StyleRoot>
+                        <Coverflow
+                            width={560}
+                            height={830}
+                            displayQuantityOfSide={.8}
+                            navigation={false}
+                            enableHeading={false}
+                            active={this.state.active}
 
-                    <img src='https://i.ibb.co/931K29J/Nobu.png' alt='Independent Filmmaker Database: JS|MongoDB|Express|React|Node' data-action="https://nobu-filmmakers.herokuapp.com" />
-                    <img src='https://i.ibb.co/w0XPx52/PicList2.png' alt='Event Management Database: JS|MongoDB|Express|Node' data-action="https://piclist.herokuapp.com" />
-                    <img src='https://i.ibb.co/HFKctrf/hhgttgweb.png' alt="Hitchhiker's Guide to The Galaxy Fansite: HTML|CSS|JS" data-action="https://hitchhiker42.netlify.com" />
-                    <img src='https://i.ibb.co/F5WT0jc/Schmalculator.png' alt='Javascript Calculator: HTML|CSS|JS' data-action="https://schmalculator.netlify.com" />
-                    {/* <img src='images/.png' alt='two' data-action="http://"/>
-              <img src='images/.png' alt='three' data-action="https://"/>
-              <img src='images/.png' alt='four' data-action="http://"/> */}
-                </Coverflow>
-                
-                </StyleRoot>
-                {/* <NextBtn>
+                        >
+
+                            <div
+                                onClick={() => fn()}
+                                onKeyDown={() => fn()}
+                                role="menuitem"
+                                tabIndex="0"
+                            >
+                                {/* Scroll or click Next button to view my work */}
+                            </div>
+
+                            <div>
+                                <img src='https://i.ibb.co/ZWD3mL4/sceneitweb.png' alt='SceneIt Pictures Interactive Website: HTML|CSS|JS' data-action="https://sceneitpix.com" />
+                                <h2>  SceneIt Pictures Interactive Website: <br /> created with: HTML | CSS | JS </h2>
+                            </div>
+
+                            <div>
+                                <img src='https://i.ibb.co/s3FcP6n/Major-Keys.png' alt='Major Keys Javacript Trivia Game: HTML|CSS|JS' data-action="https://majorkeys.netlify.com" />
+                                <h2>  Major Keys Javacript Trivia Game <br /> created with: HTML | CSS | JS  </h2>
+                            </div>
+
+                            <div>
+                                <img src='https://i.ibb.co/wY2FYNY/Callum-Ent.png' alt='Callum Enterprise website: HTML|CSS|JS' data-action="https://callumenterprise.com" />
+                                <h2> Callum Enterprise website  <br /> created with: HTML | CSS | JS </h2>
+
+                            </div>
+
+                            <div>
+                                <img src='https://i.ibb.co/DKNZsX8/CCXMobile2.png' alt='Social Media Network for Creative Freelancers: JS|Python|React|Django|SQL' data-action="https://creative-freedom.herokuapp.com" />
+                                <h2> Social Media Network for Creative Freelancers  <br /> created with: JS | Python | React | Django | SQL</h2>
+                            </div>
+
+                            <div>
+                                <img src='https://i.ibb.co/931K29J/Nobu.png' alt='Independent Filmmaker Database: JS|MongoDB|Express|React|Node' data-action="https://nobu-filmmakers.herokuapp.com" />
+                                <h2> Independent Filmmaker Database <br /> created with: JS | MongoDB | Express | React | Node </h2>
+                            </div>
+
+                            <div>
+                                <img src='https://i.ibb.co/w0XPx52/PicList2.png' alt='Event Management Database: JS|MongoDB|Express|Node' data-action="https://piclist.herokuapp.com" />
+                                <h2> PicList Employee Management Database  <br /> created with: JS | MongoDB | Express | React | Node </h2>
+                            </div>
+
+                            <div>
+                                <img src='https://i.ibb.co/HFKctrf/hhgttgweb.png' alt="Hitchhiker's Guide to The Galaxy Fansite: HTML|CSS|JS" data-action="https://hitchhiker42.netlify.com" />
+                                <h2> Social Media Network for Creative Freelancers  <br /> created with: HTML | CSS | JS</h2>
+                            </div>
+
+                            <div>
+                                <img src='https://i.ibb.co/F5WT0jc/Schmalculator.png' alt='Javascript Calculator: HTML|CSS|JS' data-action="https://schmalculator.netlify.com" />
+                                <h2> Javascript Calculator <br /> created with: HTML | CSS | JS</h2>
+                            </div>
+
+
+
+
+
+
+
+                        </Coverflow>
+
+                    </StyleRoot>
+                    {/* <NextBtn>
                         <Link to="/Skills"><button>Skills</button></Link>
 
                     </NextBtn> */}
