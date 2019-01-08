c1d2af005be15cd12694d7c8659829332b3fc124
INDIA GUY <indiaguy@SCENEITs-iMac.local>
2019-01-08 14:33:07 -0500

    test image9

--- src/components/SceneItPix.js
+++ src/components/SceneItPix.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styled from "styled-components";
 import img from '../images/SIPDeskR.png';
 import img2 from '../images/SIPBackdropL.png';
-import img3 from '../images/Final Cut Pro X.png';
+import img3 from '../images/touchHere.png';
 
 
 const Container = styled.div`
