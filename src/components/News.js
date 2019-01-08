e4bcc549b3087a059571b2010d5af7f188ee6797
INDIA GUY <indiaguy@SCENEITs-iMac.local>
2019-01-07 19:26:20 -0500

    news mobile responsive

--- src/components/News.js
+++ src/components/News.js
@@ -8,14 +8,10 @@ import { Link } from "react-router-dom";
 
 
 const Background = styled.div`
-color: white;
-text-shadow: 1px 1px 1px black;
-font-size: 25px;
-font-weight: 800;
 background-image: url(${img});
 background-repeat: no-repeat;
 background-size: contain;
-position: absolute;
+position: fixed;
 z-index: -100;
   width: 100vw;
   margin: 0;
@@ -23,8 +19,17 @@ z-index: -100;
   text-align: center;
   height: 100vw;
   h1{
-    background-color: rgba(0,0,0);
-
+      
+    color: black;
+    text-shadow: 1px 1px 1px white;
+    font-size: 25px;
+    background-color: rgba(255, 255, 255, 0.4);
+    z-index: 1000;
+    border-radius: 15px;
+        border: inset rgb(228, 228, 228)2px;
+        padding: 10px;
+        width: 100vw;
+        
 }
 `
 
@@ -63,21 +68,25 @@ button{
 }
 `
 const GirlStill1 = styled.div`
-button{
-    margin-top 0%;
-    width: 100px;
-    height: 50px;
-    position: fixed;
-    z-index: 1000;
-}
+
 `
 const GirlRun = styled.div`
-button{
-    margin-top 10%;
-    width: 100px;
-    height: 50px;
-    position: fixed;
-    z-index: 1000;
+// display: none;
+animation: 1s appear;
+animation-fill-mode: forwards;
+animation-delay: 16s;
+visibility: hidden;
+}
+
+
+
+@keyframes appear {
+99% {
+  visibility: hidden;
+}
+100% {
+  visibility: visible;
+}
 }
 `
 const Sky = styled.div`
@@ -85,6 +94,7 @@ color: red;
 font-size: 45px;
 background-image: url(${img2});
 background-size: contain;
+background-repeat-y: no-repeat;
 overflow-x: hidden;
 position: fixed;
 z-index: -200;
@@ -99,31 +109,54 @@ repeat 0 0;
 }
 `
 const Tornado = styled.div`
-    margin-top: -35%;
+    margin-top: -30%;
     position: fixed;
     z-index: -180;
     margin-left: 0%;
-    height: 100vw;
+    height: 75vw;
     width: 100vw;
     // border: 3px solid black;
     background-image: url('https://data.whicdn.com/images/308544314/original.gif');
     background-repeat: no-repeat;
     background-size: cover;
-    display: block;
-
+    overflow-y: hidden
+    animation-name: tornado;
+    animation-duration: 16s;
+    animation-fill-mode: forwards;
+    animation-timing-function: ease-out;  
+    @keyframes tornado {
+        0% {
+          opacity: 0;
+        }
+        100% {
+          opacity: 1
+        }
+      }
 
 `
 const Rain = styled.div`
 background-image: url('https://gifimage.net/wp-content/uploads/2018/04/rain-transparent-gif-9.gif');
-margin-top: -3%;
+margin-top: -1%;
 margin-left: 0%;
-position: absolute;
-z-index: 2;
-background-size: 115%;
+position: fixed;
+z-index: -2;
+background-size: 105%;
 width: 100vw;
-height: 100vh;
+height: 105vw;
 background-repeat: no-repeat;
 display: block;
+animation-name: rain;
+animation-duration: 35s;
+animation-fill-mode: forwards;
+animation-timing-function: ease-out;  
+@keyframes rain {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1
+    }
+  }
 `
 
 const Child1 = styled.div`
@@ -132,17 +165,21 @@ background-repeat: no-repeat;
 background-size: 22%;
 margin-top: 36.2%;
 margin-left: 51.7%;
-position: absolute;
+position: fixed;
 z-index: -4;
 height: 100%;
 width: 100%;
 animation: slide 5s linear 1;
+animation-delay: 17s;
 animation-fill-mode: forwards;
 
 @keyframes slide {
     from { margin-left: 50%; }
    to { margin-left: -140%;  }
 }
+
+
+
 `
 const Child2 = styled.div`
 background-image: url(${camGirl2});
@@ -150,17 +187,31 @@ background-repeat: no-repeat;
 background-size: 25%;
 margin-top: 35%;
 margin-left: 50%;
-position: absolute;
+position: fixed;
 z-index: -4;
 height: 100%;
 width: 100%;
+animation: 1s disappear;
+animation-fill-mode: forwards;
+animation-delay: 16s;
+visibility: visible;
+}
+
+@keyframes disappear {
+99% {
+  visibility: visible;
+}
+100% {
+  visibility: hidden;
+}
+}
 `
 
 const Reporter = styled.div`
 background-image: url('https://i.ibb.co/5MG8S57/reporter.png');
 margin-top: 34%;
 margin-left: 32%;
-position: absolute;
+position: fixed;
 z-index: -4;
 background-size: 20%;
 width: 80vw;
@@ -170,23 +221,119 @@ background-repeat: no-repeat;
 `
 
 const NextBtn = styled.div`
-button{
-    position: relative;
-    z-index: 1000;
-    border-radius: 9px;
-    font-size: 15px;  
-margin-top: -6em;
-margin-left: 75vw;
-background-color: rgba(255, 255, 255, 0.619);
 
+h1{
+      
+    color: black;
+    // text-shadow: 1px 1px white;
+    font-size: 25px;
+    background-color: rgba(255, 255, 255, 0.6);
+    z-index: 1000;
+    border-radius: 15px;
+        border: inset rgb(228, 228, 228)2px;
+        padding: 10px;
+        width: 100vw;
+        text-align: center;
+        margin-top: 2em;
+        // margin-left: 25vw;    
+
+}
+//really small mobile screens
+@media only screen and (min-width: 320px)    {
+        h1{
+            margin-top: 10.5em;
+            background-color: #bdd4e7;
+            background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);
+            
+            font-size: 15px;
+            color: white;
+            width:100vw;
+
+        }
+        }
+   }
+//s5
+   @media only screen and (min-width: 360px)   {
+    h1{
+        margin-top: 12em;
+        background-color: #bdd4e7;
+        background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);
+        
+        font-size: 15px;
+        color: white;
+        width:100vw;
+
+    }
+    }
+}
+//iphone6, iphoneX
+@media only screen and (min-width: 375px)   {
+    h1{
+        margin-top: 13em;
+        background-color: #bdd4e7;
+        background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);
+        
+        font-size: 15px;
+        color: white;
+        width:100vw;
+
+    }
+    }
+}
+//iphone6+
+
+@media only screen and (min-width: 414px)   {
+    h1{
+        margin-top: 14em;
+        background-color: #bdd4e7;
+        background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);
+        
+        font-size: 15px;
+        color: white;
+        width:100vw;
+
+    }
+    }
+}
+
+
+   //ipad
+   @media only screen and (min-width: 768px) {
+    h1{
+        margin-top: 27em;
+        background-color: #bdd4e7;
+        background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);
+        
+        font-size: 15px;
+        color: white;
+        width:100vw;
+
+    }
+    }
+}
+
+// ipad pro
+@media only screen and (min-width: 1024px) and (min-height: 1366px){
+    h1{
+        margin-top: 16em;
+        background-color: #bdd4e7;
+        background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);
+        
+        font-size: 35px;
+        color: white;
+        width:100vw;
+
+    }
+    }
+}
 `
 class News extends Component {
     constructor() {
         super()
         this.state = {
-            showWeather: false,
+            showWeather: true,
             girlStill1: true,
-            girlRun: false,
+            girlRun: true,
         }
     }
     operation() {
@@ -211,15 +358,9 @@ class News extends Component {
     render() {
         return (
             <div>
-                <Background><br/><br/><br/><br/>                  
-                    <NextBtn> <h1> Then, she went on to shoot live News Television...  <br/>  
-                    After working 12 hour shifts through fires and disasters and hurricanes, <br/> 
-                    she quickly realized that the News Industry was not for her.  <br/>
-                    So she ran away!</h1> 
-                    <a href='/'><button>Back</button></a> 
-                    <Link to="/ship"><button>Next Chapter</button></Link>
-                    </NextBtn>
+                <Background>
                     </Background>
+                    
                 <Sky></Sky>
 
                 <Reporter></Reporter>
@@ -248,9 +389,9 @@ class News extends Component {
                 </GirlRun>
                 <BothBtns>
                     <br/>
-                    <WeatherBtn>
-                        <button onClick={() => this.operation()}>Hurricane</button>
-                    </WeatherBtn>
+                    {/* <WeatherBtn>
+                        <button onClick={() => this.operation()}>Weather</button>
+                    </WeatherBtn> */}
                     {
                         this.state.showWeather ?
                             <div>
@@ -259,11 +400,20 @@ class News extends Component {
                             </div>
                             : null
                     }
-                    <RunBtn>
+                    {/* <RunBtn>
                         <button onClick={() => { this.operation2(); this.operation3(); }}>Run! </button>
-                    </RunBtn>
+                    </RunBtn> */}
 
                 </BothBtns>
+                             
+                    <NextBtn> <h1> Then, she went on to shoot live News Television...  <br/>  
+                    and after working 12 hour shifts through fires and disasters and hurricanes, <br/> 
+                    she quickly realized that the News Industry was not for her.  <br/>
+                    So she ran away! <br/>
+                    <a href='/about'>Back</a> <br/>
+                    <a href='/ship'>Next Chapter</a> 
+                    </h1> 
+                    </NextBtn>
             </div>
         );
     }
