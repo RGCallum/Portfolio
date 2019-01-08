7dcab6fabe0b03a969242ead3461729d55a87219
INDIA GUY <indiaguy@SCENEITs-iMac.local>
2019-01-08 14:48:13 -0500

    test image11

--- src/components/SceneItPix.js
+++ src/components/SceneItPix.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styled from "styled-components";
 import img from '../images/SIPDeskR.png';
 import img2 from '../images/SIPBackdropL.png';
-import img3 from '../images/Magic-Mirror.png';
+import img3 from '../MagicMirrorGlow.png';
 
 
 const Container = styled.div`
