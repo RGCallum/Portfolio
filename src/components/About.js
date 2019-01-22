84e02ed89374cd6ce210949562acee454d81504b
INDIA GUY <indiaguy@SCENEITs-iMac.local>
2019-01-22 13:00:45 -0500

    testing

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 const Background = styled.div`
 background-image: url(${img});
-background-size: cover;
+background-size: contain;
 background-repeat-y: no-repeat;
 color: black;
 
