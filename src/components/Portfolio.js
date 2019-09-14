c3493c425e11489e0aef901963375bb07b935d51
INDIA GUY <rguy129@yahoo.com>
2019-09-13 22:37:26 -0400

    new image for portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -155,7 +155,7 @@ export default class Portfolio extends React.Component {
                             </div>
                             <div>
                                 <a href='https://callumpay.herokuapp.com'>
-                                    <img src='https://i.ibb.co/B2xsRbH/Tenderscrnshot.png' alt='Tender Web and Mobile App: : JS|MongoDB|Express|React|Node' data-action="https://callumpay.herokuapp.com" />
+                                    <img src='https://i.ibb.co/2q16Qvt/Tenderscrnshot2.png' alt='Tender Web and Mobile App: : JS|MongoDB|Express|React|Node' data-action="https://callumpay.herokuapp.com" />
                                 </a>
                                 <h2>  "Tender" <br/> Web and Mobile App <br /> created with: React | JS | Express | MongoDB | Node  </h2>
                             </div> 
